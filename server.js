8bb86dbc4c519d9b219010ef8d59fe1573b922d3
ahjorgen167 <ahjorgen167@gmail.com>
2016-10-04 13:43:09 -0700

    Re-adding assignment 1 parts

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 var express = require('express')
 var app = express()
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('port', (process.env.PORT || 3000))
 app.use(express.static(__dirname + '/public'))
 
 //app.get('/', function(request, response) {
 //  response.send('Hello World!')
 //})
+require ("./test/app.js")(app);
+
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
